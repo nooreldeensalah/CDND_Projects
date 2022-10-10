b4bf995b52f6309852fcc3caf0e88514122501b7
Noor Eldeen Salah <nooreldeensalah99@gmail.com>
2022-10-10 08:44:23 +0200

    Export functions compliant with `todos.ts` interface

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
@@ -1,5 +1,5 @@
 import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
+import { AttachmentUtils } from './attachmentUtils'
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
@@ -8,3 +8,15 @@ import * as uuid from 'uuid'
 import * as createError from 'http-errors'
 
 // TODO: Implement businessLogic
+const logger = createLogger('todos-BusinessLogic')
+const todosAccess = new TodosAccess()
+
+export async function createTodo(params: type) {}
+
+export async function deleteTodo(params: type) {}
+
+export async function updateTodo(params: type) {}
+
+export async function getTodosForUser(params: type) {}
+
+export async function createAttachmentPresignedUrl(params: type) {}
